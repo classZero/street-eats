f5db143da0237c00ada82b3578350b4a7580dcc4
balintataw <balintataw2@gmail.com>
2018-04-27 20:02:19 -0700

    created store and home component folder as suggested template

--- client/src/store.js
+++ client/src/store.js
@@ -1,1 +1,11 @@
 import {createStore, combineReducers} from 'redux'
+
+import  homeReducer from './components/home/homeReducer'
+//import reducer here
+
+const rootReducer = combineReducers({
+  homeReducer,
+  //add your reducer here
+})
+
+const store = createStore(rootReducer)
