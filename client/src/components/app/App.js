f38333e9a166b6c1d00062d19efb99a900b1c0ca
balintataw <balintataw2@gmail.com>
2018-04-27 20:29:28 -0700

    added react react-redux and Provider to client/index.js

--- client/src/components/app/App.js
+++ client/src/components/app/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 
+import Home from '../home/Home'
+
 class App extends Component {
   render () {
     return (
       <div className='App'>
-        <h1>Hello World</h1>
+        <Router>
+          <div>
+            <Route to="/" exact component={Home} />
+          </div>
+        </Router>
       </div>
     )
   }
--- client/src/store.js
+++ client/src/store.js
@@ -9,3 +9,5 @@ const rootReducer = combineReducers({
 })
 
 const store = createStore(rootReducer)
+
+export default store
