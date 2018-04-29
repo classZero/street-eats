e079c7f3ff4c120dc4d7f9f158488d63d1c27f35
balintataw <balintataw2@gmail.com>
2018-04-29 14:07:23 -0700

    temporary push of node modules

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react'
 import {BrowserRouter as Router, Route} from 'react-router-dom'
-import {Provider} from 'react-redux'
 import store from './store.js'
 import TProfileView from './truckprofile/components/tprofileview'
+import Home from './home/Home'
 
 class App extends Component {
   render () {
     return (
-      <Provider store={store}>
         <Router>
-          <Route path="/" component={TProfileView} />
+          <div>
+            <Route path="/" exact component={Home} />
+            <Route path="/truckprofile" component={TProfileView} />
+          </div>
         </Router>
-      </Provider>
     )
   }
 }
