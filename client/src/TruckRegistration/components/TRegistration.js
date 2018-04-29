61fe6f02bff0ec72e10901f2accdb984a7d1d2bb
Chris Steinle <steinle.4400@gmail.com>
2018-04-29 13:21:18 -0700

    created Truck Registration component from User Registration component

---
+++ client/src/TruckRegistration/components/TRegistration.js
@@ -0,0 +1,54 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+
+class TRegistration extends Component {
+	state = {
+		username: '',
+		companyName: '',
+		email: '',
+		password: '',
+		confirmPassword: ''
+	}
+
+	handleChange = (e) =>{
+		e.preventDefault()
+		this.setState({
+			[e.target.name]: e.target.value
+		})
+	}
+
+	handleSubmit = (e) =>{
+		e.preventDefault()
+		// registerTruck()
+		this.setState({
+			username: '',
+			email: '',
+			password: '',
+			confirmPassword: ''
+		})
+	}
+
+	render(){
+		return(
+			<div>
+				<fieldset>
+					<legend>Food Truck Registration</legend>
+					<form onSubmit={this.handleSubmit}>
+						<input onChange={this.handleChange} type="text" name="username" value={this.state.username}  placeholder="username"/>
+						<input onChange={this.handleChange} type="text" name="companyName" value={this.state.companyname} placeholder="company name" />
+						<input onChange={this.handleChange} type="text" name="email" value={this.state.email} placeholder="email"/>
+						<input type="file" name="companyLogo" />
+						<input onChange={this.handleChange} type="password" name="password" value={this.state.password} placeholder="password"/>
+						<input onChange={this.handleChange} type="password" name="confirmPassword" value={this.state.confirmPassword} placeholder="confirm password"/>
+						<button type="submit">Register</button>
+					</form>
+				</fieldset>
+			</div>
+	)}
+}
+
+function mapStateToProps(state) {
+	return {}
+}
+
+export default connect(mapStateToProps)(TRegistration)
