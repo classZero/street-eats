546bad809c8817adf154dcd4c1ca64e9e22de797
Chris Steinle <steinle.4400@gmail.com>
2018-04-29 11:44:28 -0700

    added needed inputs to user registration component

--- client/src/UserRegistration/components/URegistration.js
+++ client/src/UserRegistration/components/URegistration.js
@@ -2,13 +2,35 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 
 class URegistration extends Component {
+	state = {
+		username: '',
+		email: '',
+		password: '',
+		confirmPassword: ''
+	}
+
+	handleChange = (e) =>{
+		this.setState({
+			[e.target.name]: e.target.value
+		})
+	}
+
+
 	render(){
 		return(
 			<div>
-				
+				<fieldset>
+					<legend>User Registration</legend>
+					<form>
+						<input onChange={this.handleChange} type="text" name="username" value={this.state.username}  placeholder="username"/>
+						<input onChange={this.handleChange} type="text" name="email" value={this.state.email} placeholder="email"/>
+						<input onChange={this.handleChange} type="password" name="password" value={this.state.password} placeholder="password"/>
+						<input onChange={this.handleChange} type="password" name="confirmPassword" value={this.state.confirmPassword} placeholder="confirm password"/>
+						<button type="submit">Register User</button>
+					</form>
+				</fieldset>
 			</div>
-		)
-	}
+	)}
 }
 
 function mapStateToProps(state) {
