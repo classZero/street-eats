b7e6fd6a009476767fbaecfdba1698b27d9f7f9c
Chris Steinle <steinle.4400@gmail.com>
2018-04-29 11:24:01 -0700

    structured user registration component

---
+++ client/src/UserRegistration/components/URegistration.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+
+class URegistration extends Component {
+	render(){
+		return(
+			<div>
+				
+			</div>
+		)
+	}
+}
+
+function mapStateToProps(state) {
+	return {}
+}
+
+export default connect(mapStateToProps)(URegistration)
