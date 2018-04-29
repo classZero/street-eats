9283fa7340668b6348d60f3099e0ac1adbf36e40
Chris Steinle <steinle.4400@gmail.com>
2018-04-29 13:03:15 -0700

    created action file for User Registration, added axios to client modules, added proxy to client package.json

---
+++ client/src/UserRegistration/actions/URegistrationActions.js
@@ -0,0 +1,6 @@
+import store from 'store'
+import axios from 'axios'
+
+export function registerUser(){
+	console.log('This will register a user')
+}
--- client/src/UserRegistration/components/URegistration.js
+++ client/src/UserRegistration/components/URegistration.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
+import {registerUser} from '../actions/URegistrationActions'
 
 class URegistration extends Component {
 	state = {
@@ -10,18 +11,29 @@ class URegistration extends Component {
 	}
 
 	handleChange = (e) =>{
+		e.preventDefault()
 		this.setState({
 			[e.target.name]: e.target.value
 		})
 	}
 
+	handleSubmit = (e) =>{
+		e.preventDefault()
+		registerUser()
+		this.setState({
+			username: '',
+			email: '',
+			password: '',
+			confirmPassword: ''
+		})
+	}
 
 	render(){
 		return(
 			<div>
 				<fieldset>
 					<legend>User Registration</legend>
-					<form>
+					<form onSubmit={this.handleSubmit}>
 						<input onChange={this.handleChange} type="text" name="username" value={this.state.username}  placeholder="username"/>
 						<input onChange={this.handleChange} type="text" name="email" value={this.state.email} placeholder="email"/>
 						<input onChange={this.handleChange} type="password" name="password" value={this.state.password} placeholder="password"/>
