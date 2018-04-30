0d88da4a624227368960a4c2b9d5886fbe8296e3
Chris Steinle <steinle.4400@gmail.com>
2018-04-30 15:43:34 -0700

    commiting app.js, to be overwritten by remote

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,19 @@ import {BrowserRouter as Router, Route} from 'react-router-dom'
 import {Provider} from 'react-redux'
 import store from './store.js'
 import TProfileView from './truckprofile/components/tprofileview'
+import URegistration from 'UserRegistration/components/URegistration'
+import TRegistration from 'TruckRegistration/components/TRegistration'
 
 class App extends Component {
   render () {
     return (
       <Provider store={store}>
         <Router>
-          <Route path="/" component={TProfileView} />
+	        <div>
+	        	<Route path="/registration" component={URegistration} />
+            <Route path="/truck" component={TRegistration} />
+	        	<Route exact path="/" component={TProfileView} />
+	        </div>
         </Router>
       </Provider>
     )
