691c62f519f932a6d15f6cc263410a7c92ff8c88
Chris Steinle <steinle.4400@gmail.com>
2018-04-30 17:16:43 -0700

    added registration routes

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Route} from 'react-router-dom'
 import store from './store.js'
 import TProfileView from './truckprofile/components/tprofileview'
 import URegistration from './UserRegistration/components/URegistration'
+import TRegistration from './TruckRegistration/components/TRegistration'
 import Home from './home/Home'
 import UProfileView from './userprofile/components/uprofileview'
 import MapViewHome from './MapView/mapviewcomponents/MapsViewHome'
@@ -15,6 +16,7 @@ class App extends Component {
             <Route exact path="/" component={Home} />
             <Route path="/truckprofile" component={TProfileView} />
             <Route path="/URegistration" component={URegistration} />
+            <Route path="/TRegistration" component={TRegistration} />
             <Route path="/userprofile" component={UProfileView} />
             <Route path="/map" component={MapViewHome} />
           </div>
