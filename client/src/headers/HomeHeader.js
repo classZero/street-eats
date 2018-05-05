afedaecc483eca84b3484ae3ab1074d9ddedcdf2
Kevin McGahey <kevinmcgahey1114@gmail.com>
2018-05-05 16:26:56 -0700

    style for edit truck page is nearly complete

--- client/src/headers/HomeHeader.js
+++ client/src/headers/HomeHeader.js
@@ -3,6 +3,7 @@ import Login from '../login/Login'
 import {connect} from 'react-redux'
 import HomeDropdown from '../dropdowns/homeDropdown/HomeDropdown'
 import trucksvg from 'assets/images/truck.svg'
+import {Link} from 'react-router-dom'
 
 export class HomeHeader extends Component {
   render() {
@@ -14,7 +15,7 @@ export class HomeHeader extends Component {
           <Login />
         </div>
 
-        <div className="logo-container"><h1 className="content-headers-title">Street Eats</h1> <img className="logo-icon" src={trucksvg} alt="logo"/></div>
+        <div className="logo-container"><Link to="/" style={{color: "inherit", cursor: "pointer"}}><h1 className="content-headers-title">Street Eats</h1></Link> <img className="logo-icon" src={trucksvg} alt="logo"/></div>
 
         <div className="home-header-dropdown">
           {this.props.username ? <p>Username : {this.props.username}</p> : ''}
