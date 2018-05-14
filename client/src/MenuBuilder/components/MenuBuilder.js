1a94f6ab887385c038c26c3ead43497e4397b58a
Chris Steinle <steinle.4400@gmail.com>
2018-05-14 10:11:52 -0700

    more styling fixes on menu builder

--- client/src/MenuBuilder/components/MenuBuilder.js
+++ client/src/MenuBuilder/components/MenuBuilder.js
@@ -56,39 +56,39 @@ class MenuBuilder extends Component{
 		          <h1>Add To Your Menu</h1>
 		          <button onClick={ e => this.props.toggle(e)} className="menubuilder-toggle">Edit Profile</button>
 		        </div>
-		    <div className="menubuilder-body-container">
-		        <div className="menubuilder-body">
-		          <form onSubmit={this.handleSubmit} id="menuBuilderForm">
-		            <h4>Item Name:</h4>
-		            <input onChange={this.handleChange} type="text" name="itemName" value={this.state.itemName} />
-		            <h4>Item Price:</h4> 
-		            <input onChange={this.handleChange} type="text" name="itemPrice" value={this.state.itemPrice} />
-		            <h4>Item Description:</h4>
-		            <input onChange={this.handleChange} type="text" name="itemDescription" value={this.state.itemDescription} />
-		            <div id="menubuilder-btn-wrapper">
-		              <h4>Item Type:</h4>
-		              <select onChange={this.handleChange} value={this.state.itemType} name="itemType">
-		                <option value="drink">Drink</option>
-		                <option value="side">Side</option>
-		                <option value="entree" >Entree</option>
-		                <option value="dessert">Dessert</option>
-		              </select>
-		            </div>
-		            <button type="submit" id="menubuilder-submit-btn">Add Item</button>
-		          </form>
-		        </div>
-	        	
-				{this.props.menu.map((item, i) => (
-				<div key={"menuitem-" + i} className="menuview-menu-item">
-	              <h3>{item.itemName}</h3>
-	              <h3>${item.itemPrice.toFixed(2)}</h3>
-	              <h4>{item.itemType}</h4>
-	              <p>{item.itemDescription}</p>
-	              <button onClick={ e => this.handleRemove(e, item.id)} className="menubuilder-remove-button">X</button>
-	            </div>	
-					)
-				)}
-			</div>
+			    <div className="menubuilder-body-container">
+			        <div className="menubuilder-body">
+			          <form onSubmit={this.handleSubmit} id="menuBuilderForm">
+			            <h4>Item Name:</h4>
+			            <input onChange={this.handleChange} type="text" name="itemName" value={this.state.itemName} />
+			            <h4>Item Price:</h4> 
+			            <input onChange={this.handleChange} type="text" name="itemPrice" value={this.state.itemPrice} />
+			            <h4>Item Description:</h4>
+			            <input onChange={this.handleChange} type="text" name="itemDescription" value={this.state.itemDescription} />
+			            <div id="menubuilder-btn-wrapper">
+			              <h4>Item Type:</h4>
+			              <select onChange={this.handleChange} value={this.state.itemType} name="itemType">
+			                <option value="drink">Drink</option>
+			                <option value="side">Side</option>
+			                <option value="entree" >Entree</option>
+			                <option value="dessert">Dessert</option>
+			              </select>
+			            </div>
+			            <button type="submit" id="menubuilder-submit-btn">Add Item</button>
+			          </form>
+			        </div>
+		        	
+					{this.props.menu.map((item, i) => (
+					<div key={"menuitem-" + i} className="menuview-menu-item">
+		              <h3>{item.itemName}</h3>
+		              <h3>${item.itemPrice.toFixed(2)}</h3>
+		              <h4>{item.itemType}</h4>
+		              <p>{item.itemDescription}</p>
+		              <button onClick={ e => this.handleRemove(e, item.id)} className="menubuilder-remove-button">X</button>
+		            </div>	
+						)
+					)}
+				</div>
 			</div>
 		)
 	}
